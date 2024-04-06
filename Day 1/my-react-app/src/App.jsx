261f196f5c7a1bcbe91ae91088a6a9b507973a75
Manmohan Aeir <manmohanaeir001110@gmail.com>
2024-04-06 22:15:56 +0545

    added another component Artical

--- Day 1/my-react-app/src/App.jsx
+++ Day 1/my-react-app/src/App.jsx
@@ -1,11 +1,13 @@
  import Header from './Header.jsx'
  import Footer from './Footer.jsx'
  import Food from './FOod.jsx';
+import Artical from './Artical.jsx';
 function App() {
 
   return( 
     <div>
       <Header />
+      <Artical />
       <Food />
       <Footer />
     </div>
---
+++ Day 1/my-react-app/src/Artical.jsx
@@ -0,0 +1,12 @@
+function Artical() {
+    return (
+         
+        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum deleniti 
+            ullam, aliquam corporis architecto, accusantium recusandae nisi voluptas porro 
+            molestias modi. Mollitia modi, saepe ex ipsa error optio nam nulla! </p>
+    )
+
+
+}
+
+export default Artical;
